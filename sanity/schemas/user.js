cdb675b13c7b67273168f45d8a40291492624e7d
한재현 <wogus0333@naver.com>
2023-10-17 18:59:36 +0900

    fix: user schema reference 주석

--- sanity/schemas/user.js
+++ sanity/schemas/user.js
@@ -23,22 +23,22 @@ export default {
       name: 'image',
       type: 'string',
     },
-    //! TODO schema 추후 작성하기
-    {
-      title: 'Notifications',
-      name: 'notifications',
-      type: 'array',
-      of: [{type: 'reference', to: [{type: 'notification'}]}], // notification type의 reference를 가질 수 있음
-      validation: (Rule) => Rule.unique(),
-    },
-    //! TODO schema 추후 작성하기
-    {
-      title: 'Bookmarks',
-      name: 'bookmarks',
-      type: 'array',
-      of: [{type: 'reference', to: [{type: 'bookmark'}]}],
-      validation: (Rule) => Rule.unique(),
-    },
+    // //! TODO schema 추후 작성하기
+    // {
+    //   title: 'Notifications',
+    //   name: 'notifications',
+    //   type: 'array',
+    //   of: [{type: 'reference', to: [{type: 'notification'}]}], // notification type의 reference를 가질 수 있음
+    //   validation: (Rule) => Rule.unique(),
+    // },
+    // //! TODO schema 추후 작성하기
+    // {
+    //   title: 'Bookmarks',
+    //   name: 'bookmarks',
+    //   type: 'array',
+    //   of: [{type: 'reference', to: [{type: 'bookmark'}]}],
+    //   validation: (Rule) => Rule.unique(),
+    // },
     // TODO 나의 게시글을 모아 볼건지?에 따라 게시글 데이터 reference 필요할 듯
   ],
 }
