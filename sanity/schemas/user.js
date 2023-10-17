a41b3c80f07195e63b8d284a9d80356ad17d9b07
한재현 <wogus0333@naver.com>
2023-10-17 18:56:33 +0900

    feat: user schema 작성

---
+++ sanity/schemas/user.js
@@ -0,0 +1,44 @@
+export default {
+  title: 'User', // sanity studio 에서 보이는 이름
+  name: 'user', // 실제 DB에서 핸들링하는 이름
+  type: 'document',
+  fields: [
+    {
+      title: 'Username',
+      name: 'username',
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    },
+    //! TODO schema 추후 작성하기
+    {
+      title: 'Notifications',
+      name: 'notifications',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'notification'}]}], // notification type의 reference를 가질 수 있음
+      validation: (Rule) => Rule.unique(),
+    },
+    //! TODO schema 추후 작성하기
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'bookmark'}]}],
+      validation: (Rule) => Rule.unique(),
+    },
+    // TODO 나의 게시글을 모아 볼건지?에 따라 게시글 데이터 reference 필요할 듯
+  ],
+}
