928d1f90a478e1d6d1beb31d2daf2c7f6a5c61e1
한재현 <wogus0333@naver.com>
2023-11-14 04:03:49 +0900

    refactor: addUser

--- src/service/user.ts
+++ src/service/user.ts
@@ -17,32 +17,14 @@ export async function addUser({ id, email, name, username, image }: OAuthUser) {
     const existingUser = await User.findOne({ socialId: id });
     if (existingUser) {
       console.log("이미 존재하는 유저", existingUser.username);
-      mongoose.connection.close();
       return true;
     }
-
-    const user = new User({
-      socialId: id,
-      username,
-      name,
-      email,
-      image,
-    });
-
-    try {
-      await user.save();
-      console.log("새로운 사용자가 생성되었습니다");
-      mongoose.connection.close();
-      return true;
-    } catch (error) {
-      console.error("사용자 생성 중 오류 발생:", error);
-      mongoose.connection.close();
-      return false;
-    }
+    return createUser({ id, email, name, username, image });
   } catch (error) {
     console.log("탐색 중 에러 error", error);
-    mongoose.connection.close();
     return false;
+  } finally {
+    mongoose.connection.close();
   }
 }
 
@@ -51,11 +33,32 @@ export async function getUserById(socialId: string) {
 
   try {
     const result = await User.findOne({ socialId });
-    mongoose.connection.close();
     return result;
   } catch (error) {
     console.log("user 정보를 찾을 수 없습니다.", error);
-    mongoose.connection.close();
     return false;
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+async function createUser({ id, email, name, username, image }: OAuthUser) {
+  const user = new User({
+    socialId: id,
+    username,
+    name,
+    email,
+    image,
+  });
+
+  try {
+    await user.save();
+    console.log("새로운 사용자가 생성되었습니다");
+    return true;
+  } catch (error) {
+    console.error("사용자 생성 중 오류 발생:", error);
+    return false;
+  } finally {
+    mongoose.connection.close();
   }
 }
